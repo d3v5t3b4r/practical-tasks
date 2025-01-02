8e7848444e6abd1ef992da2d7dc472d4425916eb
i5t4b3r <i5t4b3r@gmail.com>
2025-01-02 18:35:28 +0300

    end: lesson 2 task 2

--- src/com/walking/lesson2_operators_console_io/Task2.java
+++ src/com/walking/lesson2_operators_console_io/Task2.java
@@ -1,5 +1,7 @@
 package com.walking.lesson2_operators_console_io;
 
+import java.util.Scanner;
+
 /**
  * Введите с клавиатуры целые числа a и b.
  * Выведите на экран результат сравнения:
@@ -7,5 +9,18 @@ package com.walking.lesson2_operators_console_io;
  */
 public class Task2 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter integer a: ");
+        int a = scanner.nextInt();
+
+        System.out.print("Enter integer b: ");
+        int b = scanner.nextInt();
+
+        scanner.close();
+
+        boolean result = a * a * a > b * b;
+
+        System.out.println(result);
     }
 }
