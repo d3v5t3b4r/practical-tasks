0e457d3440d506901867989382ecb48be49de5c2
i5t4b3r <i5t4b3r@gmail.com>
2025-01-09 14:45:06 +0300

    end: lesson 3 task 3 println

--- src/com/walking/lesson3_casts_conditional_constructions/Task3Println.java
+++ src/com/walking/lesson3_casts_conditional_constructions/Task3Println.java
@@ -1,5 +1,7 @@
 package com.walking.lesson3_casts_conditional_constructions;
 
+import java.util.Scanner;
+
 /**
  * Завести три переменные типа String.
  * С клавиатуры ввести в них вашу фамилию, имя и отчество.
@@ -8,5 +10,19 @@ package com.walking.lesson3_casts_conditional_constructions;
  */
 public class Task3Println {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your first name: ");
+        String firstName = scanner.nextLine();
+
+        System.out.println("Enter your last name: ");
+        String lastName = scanner.nextLine();
+
+        System.out.println("Enter your patronymic: ");
+        String patronymic = scanner.nextLine();
+
+        scanner.close();
+
+        System.out.println(lastName + " " + firstName + " " + patronymic);
     }
 }
